39f6c5474181563bddb29adfc204c92da801e7d2
Maciek <m.p.dutkiewicz@gmail.com>
2017-08-03 13:35:35 +0200

    add jQuery version

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
 var url = 'http://api.icndb.com/jokes/random';
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-    getJoke();
+var $button = $('#get-joke').click(function() {
+	getJoke();
 });
-var paragraph = document.getElementById('joke');
+var $paragraph = $('#joke');
 function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-    xhr.send();
+    $.ajax({
+	    method: 'GET',
+	    url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
+	    success: function(response) {
+		    $paragraph.text(response.value.joke);
+	    }
+    });
 }
 getJoke();
+
+
+// var url = 'http://api.icndb.com/jokes/random';
+// var button = document.getElementById('get-joke');
+// button.addEventListener('click', function(){
+//     getJoke();
+// });
+// var paragraph = document.getElementById('joke');
+// function getJoke() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     xhr.addEventListener('load', function(){
+//     var response = JSON.parse(xhr.response);
+//     paragraph.innerHTML = response.value.joke;
+//   });
+//     xhr.send();
+// }
+// getJoke();
